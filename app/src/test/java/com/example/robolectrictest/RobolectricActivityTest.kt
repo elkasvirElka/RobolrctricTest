f70eafb11cfc0e1be8064af6be4c14cf71805f7d
Elvira <e.minnullina2015@gmail.com>
2019-07-15 12:48:13 +0300

    first commit

---
+++ app/src/test/java/com/example/robolectrictest/RobolectricActivityTest.kt
@@ -0,0 +1,43 @@
+package com.example.robolectrictest
+
+import android.widget.LinearLayout
+import android.widget.TextView
+
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.Robolectric
+import org.robolectric.RobolectricTestRunner
+import org.robolectric.annotation.Config
+
+import org.junit.Assert.assertEquals
+
+@RunWith(RobolectricTestRunner::class)
+@Config(constants = BuildConfig::class)
+class RobolectricActivityTest {
+
+    private var activity: MainActivity? = null
+
+    @Before
+    @Throws(Exception::class)
+    fun setUp() {
+        activity = Robolectric.buildActivity(MainActivity::class.java)
+            .create()
+            .resume()
+            .get()
+    }
+
+    @Test
+    //@Throws(Exception::class)
+    fun shouldHaveDefaultMargin() {
+        val textView = activity!!.findViewById(R.id.hello_textview) as TextView
+        val bottomMargin = (textView.layoutParams as LinearLayout.LayoutParams).bottomMargin
+        assertEquals(5, bottomMargin.toLong())
+        val topMargin = (textView.layoutParams as LinearLayout.LayoutParams).topMargin
+        assertEquals(5, topMargin.toLong())
+        val rightMargin = (textView.layoutParams as LinearLayout.LayoutParams).rightMargin
+        assertEquals(10, rightMargin.toLong())
+        val leftMargin = (textView.layoutParams as LinearLayout.LayoutParams).leftMargin
+        assertEquals(10, leftMargin.toLong())
+    }
+}
